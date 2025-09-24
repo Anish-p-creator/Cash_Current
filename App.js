4c833a3c293999dd234c2df4b9ae48c83c99395f
rahulg1310 <rahulganesan3@gmail.com>
2025-09-24 15:58:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CashCurrentDashboard from './CashCurrentDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CashCurrentDashboard />
+    </div>
+  );
+}
+
+export default App;
