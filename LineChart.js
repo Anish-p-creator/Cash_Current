b1dfeb1821fa618eafc5451b4b086a69f17d4d8b
rahulg1310 <rahulganesan3@gmail.com>
2025-09-24 21:59:02 +0530

    Add files via upload

---
+++ LineChart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Register the necessary components from Chart.js
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const LineChart = ({ chartData, options }) => {
+  return <Line data={chartData} options={options} />;
+};
+
+export default LineChart;
